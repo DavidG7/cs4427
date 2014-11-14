87c7f8e15abcfe54964f627df62e6e8710630977
GearoidSugrue <Gearoid.Sugrue@outlook.com>
2014-11-14 15:21:24 +0000

    Some more JUnit tests added

---
+++ src/Test/DifficultyTest.java
@@ -0,0 +1,29 @@
+package Test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DifficultyTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDifficulty() throws Exception {
+
+    }
+
+    @Test
+    public void testSetDifficulty() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDifficultyText() throws Exception {
+
+    }
+}
